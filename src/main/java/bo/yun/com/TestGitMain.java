83a5eec8956ce3e5e33bd2ccaf1c17af11a0dbbe
libo <646063013@qq.com>
2020-06-16 11:26:56 +0800

    修改001

--- src/main/java/bo/yun/com/TestGitMain.java
+++ src/main/java/bo/yun/com/TestGitMain.java
@@ -4,5 +4,6 @@ public class TestGitMain {
     public static void main(String[] args) {
         System.out.println("执行开始。。。。。。");
         System.out.println("dev执行开始。。。。。。");
+        System.out.println("dev执行完成。。。。。。");
     }
 }
